1f7437eb434aa3ae3147b1bd5e9f737b9e6b6b70
Peepow2 <85884765+Peepow2@users.noreply.github.com>
2024-01-05 22:31:41 +0700

    Create Install_package.py

---
+++ Install_package.py
@@ -0,0 +1,8 @@
+import subprocess
+List_package_name = ['fpdf2', ...]
+for pack in List_package_name:
+    try:
+        subprocess.check_call(['pip', 'install', pack])
+        print("Successfully installed", pack)
+    except subprocess.CalledProcessError as e:
+        print("Error installing", pack, e)
