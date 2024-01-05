8162ea2b71884657ba3f3c849c5c51f7c1d16a14
Peepow2 <85884765+Peepow2@users.noreply.github.com>
2024-01-05 22:33:38 +0700

    Update Install_package.py

--- Install_package.py
+++ Install_package.py
@@ -1,8 +1,19 @@
 import subprocess
 List_package_name = ['fpdf2', ...]
-for pack in List_package_name:
-    try:
-        subprocess.check_call(['pip', 'install', pack])
-        print("Successfully installed", pack)
-    except subprocess.CalledProcessError as e:
-        print("Error installing", pack, e)
+def Install_Package():
+    for pack in List_package_name:
+        try:
+            subprocess.check_call(['pip', 'install', pack])
+            print("Successfully installed", pack)
+        except subprocess.CalledProcessError as e:
+            print("Error installing", pack, e)
+    return 
+
+def Uninstall_Package():
+    for pack in List_package_name:
+        try:
+            subprocess.check_call(['pip', 'uninstall', pack, '-y'])
+            print("Successfully uninstalled", pack)
+        except subprocess.CalledProcessError as e:
+            print("Error uninstalling", pack, e)
+    return 
