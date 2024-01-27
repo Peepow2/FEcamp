8b77d05d7dc9ef7ad0de519960ad20357d8accc6
Peepow2 <85884765+Peepow2@users.noreply.github.com>
2024-01-27 22:01:29 +0700

    Update Thai_sort.py

--- Thai_sort.py
+++ Thai_sort.py
@@ -1,5 +1,11 @@
 import locale
 locale.setlocale(locale.LC_ALL, 'th_TH.utf8')
 NAME = ['โอนาน่า', 'เดเกอา', 'ฟาน เดอร์ ซาร์', 'ชไมเคิล', 'บายินเดียร์']
-Sorted_NAME = sorted(NAME, key = locale.strxfrm)
+
+# ------------------------------------------------------------------------ #
+Sorted_NAME = sorted(NAME, key = locale.strxfrm) # เรียงตามพจนานุกรม
 print(Sorted_NAME)
+
+Sorted_NAME = sorted(NAME) # เรียงตาม...
+print(Sorted_NAME)
+# ------------------------------------------------------------------------ #
